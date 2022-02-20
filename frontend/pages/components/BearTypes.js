9098161f47aa7d2aa4e72c4a18c9d1ceff23588f
jdtan <jdtan33@gmail.com>
2022-02-20 00:33:34 -0800

    :calling: Add timeline frontend

---
+++ frontend/pages/components/BearTypes.js
@@ -0,0 +1,10 @@
+const BearTypes = [
+  { name: "friendly", pos: [30, 44] },
+  { name: "slouching", pos: [59, 38] },
+  { name: "training", pos: [23, 60], size: 2.5 },
+  { name: "optimistic", pos: [45, 62], size: 2.3 },
+  { name: "rich", pos: [65, 75], size: 2.8 },
+  { name: "modern", pos: [71, 24], size: 2.6 },
+];
+
+export default BearTypes;
--- frontend/pages/components/Landing.js
+++ frontend/pages/components/Landing.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import Timer from "./Timer";
 import RoomComponent from "./Room";
+import TimelineComponent from "./TimeLine";
 import Button from "./PrimaryButton";
 import DisplaySize from "./DisplaySize";
 
@@ -30,6 +31,7 @@ const Landing = ({}) => {
   const [hasStarted, setHasStarted] = useState(false);
   const [timeLeft, setTimeLeft] = useState(startingTime); //25 minute default
   const [playMusic, setPlayMusic] = useState(false);
+  const [open, setOpen] = useState(false);
 
   const handleTimesUp = () => {
     console.log("hi");
@@ -50,6 +52,14 @@ const Landing = ({}) => {
     setPlayMusic(!playMusic);
   };
 
+  const handleTimeLineClick = () => {
+    setOpen(!open);
+  };
+
+  const handleClose = (value) => {
+    setOpen(value);
+  };
+
   return (
     <>
       <TimerContainer>
@@ -60,6 +70,7 @@ const Landing = ({}) => {
         ></Timer>
       </TimerContainer>
       <RoomComponent bears={[0, 1, 2, 3, 4, 5]} />
+      <TimelineComponent visible={open} close={handleClose} />
       <ButtonContainer>
         <StyledButton
           primary={false}
@@ -67,7 +78,12 @@ const Landing = ({}) => {
           icon={`${playMusic ? "rain-" : ""}cloud`}
           onClick={handleMusicClick}
         />
-        <StyledButton primary={false} content={"timeline"} icon={"calendar"} />
+        <StyledButton
+          primary={false}
+          content={"timeline"}
+          icon={"calendar"}
+          onClick={handleTimeLineClick}
+        />
         <StyledButton
           content={"start"}
           icon={"arrow"}
